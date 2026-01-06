375c025a5aba17b3752542f08e7ade451e7d5f4c
BaoDinh <dinhgiabaojkl@gmail.com>
2026-01-06 15:33:46 +0700

    change layout

--- src/main/java/com/example/treevisualize/Model/Description/SplayTreeDescription.java
+++ src/main/java/com/example/treevisualize/Model/Description/SplayTreeDescription.java
@@ -10,7 +10,7 @@ public class SplayTreeDescription extends TreeDescription {
     @Override
     protected String getDefinition() {
         return "A Splay Tree is a self-adjusting binary search tree with the additional property " +
-                "that recently accessed elements are quick to access again.\n\n" +
+                "that recently accessed elements are quick to access again.\n" +
                 "Key Feature: Splaying\n" +
                 "Every operation (Insert, Search, Delete) moves the accessed node to the root " +
                 "using a sequence of rotations (Zig, Zig-Zig, Zig-Zag). This tends to keep " +
@@ -22,7 +22,6 @@ public class SplayTreeDescription extends TreeDescription {
         return """
                - Average (Amortized): O(log n)
                - Worst Case: O(n) (linear, e.g., accessing elements in sequential order)
-               
                *Note: While a single operation can be slow (O(n)), a sequence of M operations 
                takes O(M log n) time, ensuring good long-term performance.*""";
     }
